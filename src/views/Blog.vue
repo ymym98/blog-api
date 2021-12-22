76ed09c0e5ea8078cc58cf2fd5a9852186cdfc05
ymym98 <jimboyumi@gmail.com>
2021-12-22 11:01:09 +0900

    APIから情報を取得し、タイトルとボディを表示させました

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,12 +1,45 @@
 <template>
-  <div></div>
+  <div>
+    <div v-for="post of postsList" v-bind:key="post.id">
+      タイトル：<br />
+      {{ post.title }}
+      <br />
+      ボディ：<br />
+      {{ post.body }}
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import { Posts } from "@/types/Posts";
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class Blog extends Vue {}
+export default class Blog extends Vue {
+  // postsの一覧を格納する配列
+  private postsList = new Array<Posts>();
+  private postsTitle = "";
+  private postsBody = "";
+
+  /**
+   * vuexのaction経由で非同期でAPIを取得する.
+   *
+   * @remarks
+   * この画面が表示されたタイミングでvuexのactionsを呼ぶ（dispatchする）
+   * ライフサイクルフックのcreatedイベント利用。
+   * 取得してからゲットするため、async awaitを利用している。
+   */
+  async created(): Promise<void> {
+    await this["$store"].dispatch("getPostsList");
+    // postListにPostsの一覧情報を格納
+    this.postsList = this["$store"].getters.getPosts;
+  }
+
+  //   showPosts() {
+  //     for(let post of this.postsList){
+
+  //     }
+}
 </script>
 
 <style></style>
